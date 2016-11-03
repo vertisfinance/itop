9be9f733575324e2f1d5de2222e49e1622b44565
Gabor Egyed <gabor.egyed@vertis.com>
2016-11-03 08:57:08 +0100

    mysql connect problem

--- docker/mysql/docker-entrypoint.sh
+++ docker/mysql/docker-entrypoint.sh
@@ -112,6 +112,17 @@ if [ "$1" = 'mysqld' -a -z "$wantHelp" ]; then
 		fi
 
 		if [ "$MYSQL_USER" -a "$MYSQL_PASSWORD" ]; then
+			echo "CREATE USER '$MYSQL_USER'@'itop.vertis.com' IDENTIFIED BY '$MYSQL_PASSWORD' ;" | "${mysql[@]}"
+
+			if [ "$MYSQL_DATABASE" ]; then
+				echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'itop.vertis.com' ;" | "${mysql[@]}"
+				echo "GRANT SUPER ON *.* TO '$MYSQL_USER'@'itop.vertis.com' ;" | "${mysql[@]}"
+			fi
+
+			echo 'FLUSH PRIVILEGES ;' | "${mysql[@]}"
+		fi
+
+		if [ "$MYSQL_USER" -a "$MYSQL_PASSWORD" ]; then
 			echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" | "${mysql[@]}"
 
 			if [ "$MYSQL_DATABASE" ]; then
