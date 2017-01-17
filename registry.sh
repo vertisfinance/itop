b0807d1110cc51b6599722a2c539845c421a3f95
Gabor Egyed <gabor.egyed@vertis.com>
2017-01-17 08:43:53 +0100

    registry.sh upgrade

--- registry.sh
+++ registry.sh
@@ -13,9 +13,9 @@ if [ [${BRANCH}] = "[master]" ]; then
       for docker_image in itop mysql
       do
         if [ [$docker_image] = "itop" ]; then
-          docker build -t $docker_image -f "$PROJECT_ROOT/vertis-docker/itop/Dockerfile" .
+          docker build -t $docker_image -f "$PROJECT_ROOT/docker/itop/Dockerfile" .
         else
-          docker build -t $docker_image "$PROJECT_ROOT/vertis-docker/mysql/"
+          docker build -t $docker_image "$PROJECT_ROOT/docker/mysql/"
         fi
           docker tag $docker_image vertis-docker-registry:5000/$docker_image:latest
           docker tag $docker_image vertis-docker-registry:5000/$docker_image:${timestamp}
