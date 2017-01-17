ec05e059f1f226910407befb4428f4b2b3d766b2
Gabor Egyed <gabor.egyed@vertis.com>
2017-01-17 08:42:51 +0100

    registry.sh upgrade

--- registry.sh
+++ registry.sh
@@ -1,20 +1,34 @@
-#!/bin/bash
-
+#!bin/sh
 set -e
 
-timestamp=$(date +"%Y-%m-%d")
+timestamp=$(date +"%Y-%m-%d-%H-%M")
+PROJECT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+BRANCH=`git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
+STATUS=`git status 2> /dev/null | grep 'working directory clean' | awk '{print $6}'`
+PUSH=`git diff origin/master | grep diff | head -1 | awk '{print $1}'`
 
-echo "pricefetch"
-echo "--------------------"
-docker tag vertis-docker-registry:5000/itop-mysql vertis-docker-registry:5000/itop-mysql:${timestamp}
-
-echo "sshd"
-echo "--------------------"
-docker tag vertis-docker-registry:5000/itop-itop vertis-docker-registry:5000/itop-itop:${timestamp}
-
-echo "Push docker-regitry"
-echo "--------------------"
-docker push vertis-docker-registry:5000/itop-itop:latest
-docker push vertis-docker-registry:5000/itop-mysql:latest
-docker push vertis-docker-registry:5000/itop-itop:${timestamp}
-docker push vertis-docker-registry:5000/itop-mysql:${timestamp}
+if [ [${BRANCH}] = "[master]" ]; then
+  if [ [${STATUS}] = "[clean]" ]; then
+    if [ [${PUSH}] != "[diff]" ]; then
+      for docker_image in itop mysql
+      do
+        if [ [$docker_image] = "itop" ]; then
+          docker build -t $docker_image -f "$PROJECT_ROOT/vertis-docker/itop/Dockerfile" .
+        else
+          docker build -t $docker_image "$PROJECT_ROOT/vertis-docker/mysql/"
+        fi
+          docker tag $docker_image vertis-docker-registry:5000/$docker_image:latest
+          docker tag $docker_image vertis-docker-registry:5000/$docker_image:${timestamp}
+          docker push vertis-docker-registry:5000/$docker_image:latest
+          docker push vertis-docker-registry:5000/$docker_image:${timestamp}
+          docker rmi vertis-docker-registry:5000/$docker_image:${timestamp}
+      done
+    else
+      echo "PLEASE PUSH ORIGIN/MASTER"
+    fi
+  else
+    echo "PLEASE COMMIT THE CHANGE"
+  fi
+else
+  echo "PLEASE CHANGE BRANCH (MASTER)"
+fi
