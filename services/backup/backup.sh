cb4e800719e83a6d17f29f208713d136df081776
gegyed <gabor.egyed@vertis.com>
2019-05-30 11:02:22 +0200

    backup perrmissin fix

--- services/backup/backup.sh
+++ services/backup/backup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 
+. /utils.sh
 
 mysqldump --host mysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DATABASE" > /backup/itop-$(date -u +"%Y-%m-%d-%H-%M-%Z").sql
-
+set_backup_perms
 find /backup -mtime +${BACKUP_RETENTION_DAYS:-1000} -delete
